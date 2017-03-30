294acbd0187bd92191571d4de47c64df52245b8e
Jamie Stevens <ste616@gmail.com>
2017-03-30 23:03:12 +1100

    Silly me, I had forgotten to make a setup.py to let other people install the library. Fixed.

---
+++ python/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+# This is the atca_rapid_response_api Python library.
+# Jamie Stevens 2017
+# ATCA Senior Systems Scientist
+# Jamie.Stevens@csiro.au
+
+setup(name='atca_rapid_response_api',
+      version='1.0',
+      description='ATCA Rapid Response Mode API',
+      url='https://github.com/ste616/atca-rapid-response-api',
+      author='Jamie Stevens',
+      author_email='Jamie.Stevens@csiro.au',
+      license='MIT',
+      packages=[ 'atca_rapid_response_api' ],
+      install_requires=[
+          'requests'
+      ],
+      zip_safe=False)
+
+# Changelog:
