7f0da4807ba3a9fdeb781c500c8bd944aaa82a2a
Jamie Stevens <ste616@gmail.com>
2022-07-14 16:01:56 +1000

    Add an example of how to cancel a request, and update the multi-freq example

--- python/example3_multifreq.py
+++ python/example3_multifreq.py
@@ -11,7 +11,7 @@ import atca_rapid_response_api as arrApi
 # Example 3.
 # The situation is the same as in example 1 of the CABB scheduling library example 1.
 # Suppose an event trigger has been received for a flaring magnetar at
-# coordinates RA = 01:00:43.1, Dec = -72:11:33.8.
+# coordinates RA = 08:00:43.1, Dec = -72:11:33.8.
 
 rq = { "source": "magnetar", "rightAscension": "08:00:43.1", "declination": "-72:11:33.8",
        "project": "C007", "maxExposureLength": "12:00:00", "minExposureLength": "3:00:00" }
@@ -33,7 +33,7 @@ rapidObj['email'] = "Jamie.Stevens@csiro.au"
 rapidObj['test'] = True
 rapidObj['emailOnly'] = "Jamie.Stevens@csiro.au"
 rapidObj['noScoreLimit'] = True
-#rapidObj['noEmail'] = True
+rapidObj['noEmail'] = True
 
 # Send the request.
 request = arrApi.api(rapidObj)
