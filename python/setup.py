d80ea77f9239312ac32db1bc0262429ea6a14147
Jamie Stevens <Jamie.Stevens@csiro.au>
2023-10-09 10:38:42 +1100

    Bump the version number for the library

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 # Jamie.Stevens@csiro.au
 
 setup(name='atca_rapid_response_api',
-      version='1.2',
+      version='1.3',
       description='ATCA Rapid Response Mode API',
       url='https://github.com/ste616/atca-rapid-response-api',
       author='Jamie Stevens',
@@ -22,4 +22,4 @@ setup(name='atca_rapid_response_api',
 # 2017-04-04, v1.1: Prevented some SSL checks, because namoi's SSL is still a little
 #   flaky.
 # 2017-04-10, v1.2: Added the maximumLag option.
-
+# 2023-10-09, v1.3: Can now use HTTP authentication to the service
